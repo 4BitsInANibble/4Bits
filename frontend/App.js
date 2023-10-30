fbe63dd2aa530290890395e96e92c96597f8afb0
nashrazaman <nz2065@nyu.edu>
2023-10-29 20:13:38 -0400

    toolbar

--- frontend/App.js
+++ frontend/App.js
@@ -1,20 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <SafeAreaView style={styles.container}>
+    <View style={styles.toolbarView}></View>
+    <ScrollView style={styles.buttonListView}></ScrollView>
+  </SafeAreaView>
+    // <View style={styles.container}>
+    //   <Text>Nibble</Text>
+    //   <StatusBar backgroundColor = "#16a085" animated = {true}/>
+    // </View>
   );
+
+
 }
 
+// const Toolbar = () => {
+//   return <SafeAreaView style={styles.container}>
+//     <View style={styles.toolbarView}></View>
+//   </SafeAreaView>
+// }
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#f5f5dc',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
+  toolbarView: {
+    width: 340,
+    height: 66,
+    backgroundColor: 'white',
+    shadowColor: 'grey',
+    shadowOffset: {width: 0, height: 8},
+    shadowOpacity: 0.4,
+    shadowRadius: 10,
+    borderRadius: 12,
+    marginHorizontal: 24,
+    marginVertical: 40,
   },
+  buttonListView:{
+    position='absolute',
+    height: 66,
+    width= '100%',
+    marginHorizontal: 24,
+    marginVertical: 40,
+  }
 });
+
