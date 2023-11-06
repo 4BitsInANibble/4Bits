5e3adb39f76580c4a0f9c7a52400b6b4349dc4cd
Gayatri Tyagi <gt2126@nyu.edu>
2023-11-05 23:55:32 -0500

    updated test file and added get pantry tests

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -1,5 +1,6 @@
 import server.endpoints as ep
-
+import random
+from string import ascii_uppercase
 
 TEST_CLIENT = ep.app.test_client()
 
@@ -9,3 +10,47 @@ def test_hello():
     resp_json = resp.get_json()
     print(f'{resp_json=}')
     assert ep.HELLO_RESP in resp_json
+
+def test_get_users():
+    resp = TEST_CLIENT.get(ep.USERS_EP)
+    resp_json = resp.get_json()
+    assert isinstance(resp_json[ep.DATA], dict)
+
+def test_get_user_valid():
+    username = 'cc6956'
+    resp = TEST_CLIENT.get(f'{ep.USERS_EP}/{username}')
+    print(f'{ep.USERS_EP}/{username}')
+    print(f'{resp=}')
+    resp_json = resp.get_json()
+    print(f'{resp_json=}')
+    usr = resp_json[ep.DATA]
+    print(f'{usr=}')
+    assert isinstance(usr, dict)
+    assert usr != {}
+    assert resp_json[ep.USER_EXISTS]
+
+def test_get_user_invalid():
+    username = ''.join(random.choices(ascii_uppercase, k=6))
+    resp = TEST_CLIENT.get(f'{ep.USERS_EP}/{username}')
+    print(f'{ep.USERS_EP}/{username}')
+    print(f'{resp=}')
+    resp_json = resp.get_json()
+    print(f'{resp.json=}')
+    usr = resp_json[ep.DATA]
+    print(f'{usr=}')
+    assert isinstance(usr, dict)
+    assert usr == {}
+    assert not resp_json[ep.USER_EXISTS]
+
+def test_get_pantry_valid():
+    username = 'cc6956'
+    resp = TEST_CLIENT.get(f'{ep.USERS_EP}/{username}/{ep.PANTRY_EP}')
+    resp_json = resp.get_json()
+    pantry_owner = resp_json[ep.PANTRY_OWNER]
+    assert pantry_owner == username
+
+def test_get_pantry_invalid():
+    username = ''.join(random.choices(ascii_uppercase, k=6))
+    resp = TEST_CLIENT.get(f'{ep.USERS_EP}/{username}/{ep.PANTRY_EP}')
+    resp_json = resp.get_json()
+    assert not resp_json[ep.USER_EXISTS]
