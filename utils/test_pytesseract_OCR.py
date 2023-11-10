487c474d8c0e80dea455ad9e5703b0143f18c463
JasonHysy <yh3595@nyu.edu>
2023-11-10 02:15:04 -0500

    fancier testing for test_pytesseract_OCR

--- utils/test_pytesseract_OCR.py
+++ utils/test_pytesseract_OCR.py
@@ -1,24 +1,47 @@
-import unittest
+
+import pytest
 from PIL import Image
 import pytesseract
 from pytesseract_OCR import ocr_image
 
+# A fixture to set up a test image path
+@pytest.fixture
+def test_image_path():
+    #need to replace 'example_receipt.jpg' with a valid test image path in your test directory
+    return 'example_receipt.jpg'
 
-# A test case for the ocr_image function
-class TestOCR(unittest.TestCase):
-    def test_ocr_image(self):
-        # You need to replace 'test_image.jpg' with a valid test image path in your test directory
-        test_image_path = 'example_receipt.jpg'
-        
-        # Call the function with the test image
-        result = ocr_image(test_image_path)
-        
-        # Check if the result is a string
-        self.assertIsInstance(result, str, "The OCR result should be a string.")
-        
-        # Check if the result is not empty
-        self.assertTrue(result.strip(), "The OCR result should not be an empty string.")
+# A test case for the ocr_image function using the fixture
+def test_ocr_image(test_image_path):
+    # Call the function with the test image
+    result = ocr_image(test_image_path)
+    
+    # Check if the result is a string
+    assert isinstance(result, str), "The OCR result should be a string."
+    
+    # Check if the result is not empty
+    assert result.strip(), "The OCR result should not be an empty string."
 
-# This allows the test to be run from the command line
-if __name__ == '__main__':
-    unittest.main()
+# Example of using `with raises` to check for a specific exception
+def test_ocr_failure():
+    with pytest.raises(Exception):
+        # This should raise an exception because the path is invalid
+        ocr_image('invalid_path.jpg')
+
+# Example of using `skip`
+@pytest.mark.skip(reason="this test is not applicable at the moment")
+def test_ocr_skip():
+    # Test code that should be skipped
+    pass
+
+# Example of using `patch` to mock pytesseract.image_to_string
+def test_ocr_mocked(mocker):
+    # Mock pytesseract.image_to_string to return a predefined string
+    mocked_ocr = mocker.patch('pytesseract.image_to_string', return_value='mocked result')
+    
+    # Call the function with any image since it's mocked
+    result = ocr_image('any_image.jpg')
+    
+    # Assert the result is what we mocked
+    assert result == 'mocked result'
+    # Ensure the mock was called
+    mocked_ocr.assert_called_once()
