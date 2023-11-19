ae7979e456e6461aaaf30c4a649dd88b6172121f
JasonHysy <yh3595@nyu.edu>
2023-11-19 18:22:21 -0500

    added mongo db credentials to os variables

--- local.sh
+++ local.sh
@@ -3,6 +3,8 @@
 export FLASK_ENV=development
 export PROJ_DIR=$PWD
 export DEBUG=1
+export MONGO_DB_USRNAME=nz2065
+export MONGO_DB_PASSWORD=Cbf6EvI2pewBj8KM
 
 # run our server locally:
 PYTHONPATH=$(pwd):$PYTHONPATH
